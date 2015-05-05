a10b1fd6425dadb8ca4529640ff6cd0db009bf9f
Müller Urs <Mller Urs@10.3.98.10>
2015-05-05 16:38:44 +0200

    gugusali auskommentiert

--- DotsandBoxes/src/ch/hslu/prg2/controlling/GameController.java
+++ DotsandBoxes/src/ch/hslu/prg2/controlling/GameController.java
@@ -16,7 +16,7 @@ public class GameController {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        Guguisäli
+        //Guguisäli
     }
     
 }
