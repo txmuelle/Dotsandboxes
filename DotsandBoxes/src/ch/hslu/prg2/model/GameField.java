d1179b1cf2ae9cff230d331b5d3aa50c00ce645a
jordanklaus <jordanklaus@klausmbp>
2015-05-11 18:57:54 +0200

    SetLine-Methode angepasst und checkBoxes-Methode neu erstellt

--- DotsandBoxes/src/ch/hslu/prg2/model/GameField.java
+++ DotsandBoxes/src/ch/hslu/prg2/model/GameField.java
@@ -65,6 +65,15 @@ public class GameField implements IGameField {
     
     public void setLine(int row, int column, Player p){
         linematrix[row][column].setOwner(p);
+        //Setzen des Counters ,der von der Linie abhänigigen Boxen. 
+        if(row %2 == 0){//  Überprüfung, ob eine horizontale Linie ist.
+            checkBoxes(row-1,column,p);
+            checkBoxes(row+1,column,p);   
+        }else{
+            checkBoxes(row,column-1,p);
+            checkBoxes(row,column+1,p);
+        }
+    
     }
     
     
@@ -75,16 +84,41 @@ public class GameField implements IGameField {
     public boolean isLineDrawed(int row, int column){
         return !linematrix[row][column].isEmpty();
     }
+    /*  Überprüft, ob die Box überhaupt vorhanden ist und erhöht den Zähler der Box
+        um eins. Falls die Box komplett umschlossen ist, wird der Besitzer und die Farbe der Box
+        gesetzt.
     
+    */
+    public void checkBoxes(int row,int column,Player p){
+        if(row > 0 && row < matrixSize-1 && column>0 && column<matrixSize-1){
+            Boxes box = boxmatrix[row][column];
+            int counter = box.getCounter()+1;
+            box.setCounter(counter);
+            //Überprüfen, ob Quadrat geschlossen ist
+            if(counter==4){
+               box.setOwner(p);
+               box.setBoxColor(p.getColor());
+               p.setScore(p.getScore()+1);
+            }
+        }
+    }
  
     
     public static void main(String [] args){
-        GameField g = new GameField(1);
+        GameField g = new GameField(2);
         Player p = new Player("Lucius", Color.BLACK);
-        g.linematrix[1][2].setOwner(p);
         g.linematrix[3][0].setOwner(p);
-        System.out.println(g.isLineDrawed(1, 2));
         System.out.println(g.isLineDrawed(3, 0));
+        g.setLine(0, 1, p);
+        g.setLine(1, 2, p);
+        Boxes b1= g.boxmatrix[1][1];
+        Boxes b2= g.boxmatrix[1][3];
+        System.out.println(b1.getCounter());
+        g.setLine(2,1, p);
+        g.setLine(1,0, p);
+        System.out.println(b1.getCounter());
+        System.out.println(b2.getCounter());
+        System.out.println(b1.getOwner());
     }
     
 }
