32a0e9b5211343805c70597cd1a6bff8fb60cb6a
Adrian <Adrian@10.3.100.84>
2015-05-12 16:41:00 +0200

    controller angepasst

--- DotsandBoxes/src/ch/hslu/prg2/controlling/Controller.java
+++ DotsandBoxes/src/ch/hslu/prg2/controlling/Controller.java
@@ -1,12 +1,16 @@
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 package ch.hslu.prg2.controlling;
+
 import ch.hslu.prg2.model.GameField;
 import ch.hslu.prg2.model.Player;
 import ch.hslu.prg2.view.GUI;
+import java.awt.Color;
+
 
 
 /**
@@ -14,51 +18,54 @@ import ch.hslu.prg2.view.GUI;
  * @author Adrian Ruckli
  */
 public class Controller {
-    
+
     private GUI gui;
-    private GameField field;
-    private Player player1;
-    
-    public Controller(){
-        field = new GameField();
-        player1 = new Player();
+    private GameField gameField;
+    private Player player1, player2;
+    private Color color1, color2;
+    private GameVariant gameVariant;
+    private RandomKI randomKi;
+
+    public Controller() {
+        GUI gui = new GUI(gameField, this, player1, player2, gameVariant);
     }
-    
-    public void setLine(int row, int colum, Player player){
-        if(field.isLineDrawed(row, colum)){
-        }else{
-                gui.drawLine(row,colum,field.getColor());
-                }
+   
+    public boolean move(int row, int colum, Object reference) {
+        if(player1.getReference().isEquals(reference) && player1.getIsActive()){
             
-        if(colum % 2 == 0){
-            if(colum-1 > 0){
-                if(field.isBoxFull(row, colum-1)){
-                    gui.drawBox(row, colum-1, field.getColor());
-                }
-            }else if(colum+1 <= field.getMatrixSize()-1){
-                if(field.isBoxFull(row, colum+1)){
-                    gui.drawBox(row, colum+1, field.getColor());
-                }
-            }
-        }else{
-            if(row-1 > 0){
-                if(field.isBoxFull(row-1, colum)){
-                    gui.drawBox(row-1, colum, field.getColor());
-                }
-            }else if(row+1 <= field.getMatrixSize()-1){
-                if(field.isBoxFull(row+1, colum)){
-                    gui.drawBox(row+1, colum, field.getColor());
-                }
-            }
+        }
+        
+        if(gameField.isLineDrawed(row, colum)){
+            return false;
+        } else{
+            gameField.setLine(row, colum, player1);   
+            gui.update();
+            nextMove();   
+            return true;
         }
     }
-    
-    public void newGame(int size){
-        
+
+    private void nextMove() {
+        if(player1.getScore() + player2.getScore() <= (gameField.getMatrixSize()-1)/2){
+            
+        }
     }
-    
-    public static void main(String[] args){
-        GUI gui = new GUI();
-        Controller controllers = new Controller();
+
+    public void startGame(int gameModi, int size, String player1, String player2) {
+        color1 = new Color(255,0,0);
+        color2 = new Color(0,0,255);
+        
+        gameField = new GameField(size);
+        gameVariant = new GameVariant(gameModi);
+        randomKi = new RandomKI();
+        
+        this.player1 = new Player(player1, color1, randomKi);
+        this.player2 = new Player(player2, color2, gui);
+        
+        gui.update();
+    }
+
+    public static void main(String[] args) {
+        Controller controller = new Controller();   
     }
 }
