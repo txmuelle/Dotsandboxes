0c8cabc2fc002a1666b0b9cd328d168deb78ae43
Adrian <Adrian@10.3.100.84>
2015-05-12 16:44:37 +0200

    GameVariant erstellt

---
+++ DotsandBoxes/src/ch/hslu/prg2/controlling/GameVariant.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.hslu.prg2.controlling;
+
+/**
+ *
+ * @author Patrick
+ */
+public class GameVariant {
+    private int gameModi;
+    
+    public GameVariant(int gameModi){
+        this.gameModi = gameModi;
+    }
+    
+    public void setModi(int gameModi){
+        this.gameModi = gameModi;
+    }
+    
+    public int getModi(){
+        return gameModi;
+    }
+}
