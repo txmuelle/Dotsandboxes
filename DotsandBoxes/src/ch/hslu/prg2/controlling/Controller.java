487ae580856435ed68339a43a5727bdfff9c605e
Müller Urs <Mller Urs@192.168.1.56>
2015-05-18 20:56:31 +0200

    kleine Anpassung im Controlller fürs Gui

--- DotsandBoxes/src/ch/hslu/prg2/controlling/Controller.java
+++ DotsandBoxes/src/ch/hslu/prg2/controlling/Controller.java
@@ -25,17 +25,19 @@ public class Controller {
     private Player player1, player2;
     private GameVariant gameVariant;
     private RandomKI randomKi;
+    private boolean GameOver;
 
     public Controller() {
-        
-        startGame(0, 3, "A", "B");
+
+        startGame(1, 3, "A", "B");
     }
 
     /**
-     * Führt einen Spielzug aus. Überprüft wer den Zug machen will und ob dieser 
+     * Führt einen Spielzug aus. Überprüft wer den Zug machen will und ob dieser
      * an der Reihe ist. Wenn sich der Score des Spielers nach dem Zug nicht
      * verändert wird der aktuelle Player auf inaktiv gesetzt. Wenn sich der
      * Score verändert bleibt er aktiv und kann noch einen Zug machen.
+     *
      * @param row
      * @param colum
      * @param reference
@@ -46,30 +48,34 @@ public class Controller {
             if (player1.getReference() == reference && player1.getIsActive()) {
                 score = player1.getScore();
                 gameField.setLine(row, colum, player1);
-                if(score == player1.getScore()){
+                if (score == player1.getScore()) {
                     player1.setIsActive(false);
                     player2.setIsActive(true);
-                }else{}
-                
+                } else {
+                }
+
             } else if (player2.getReference() == reference && player2.getIsActive()) {
                 score = player2.getScore();
                 gameField.setLine(row, colum, player2);
-                if(score == player2.getScore()){
+                if (score == player2.getScore()) {
                     player2.setIsActive(false);
                     player1.setIsActive(true);
-                } else{}
-                
-            } else {}
+                } else {
+                }
+
+            } else {
+            }
             this.gui.update();
             nextMove();
-        } else {}
+        } else {
+        }
     }
 
     /**
      * Führt den nächsten Spielzug aus. Überprüft welcher Spieler an der Reihe
      * ist und führt je nach Klasse des aktuellen Spielers eine andere Aktion
      * aus: Gui -> keine Aktion (Methode Beenden und auf nächste Eingabe warten)
-     *      KI  -> Die KI führt einen Zug aus Es wird ausserdem überprüft ob die
+     * KI -> Die KI führt einen Zug aus Es wird ausserdem überprüft ob die
      * maximale Punktzahl erreicht wurde.
      */
     private void nextMove() {
@@ -79,7 +85,7 @@ public class Controller {
         } else if (player2.getIsActive()) {
             activePlayer = player2.getReference();
         }
-        if (player1.getScore() + player2.getScore() < Math.pow((gameField.getMatrixSize() - 1) / 2,2)) {
+        if (player1.getScore() + player2.getScore() < Math.pow((gameField.getMatrixSize() - 1) / 2, 2)) {
             if (activePlayer == gui) {
 
             } else if (activePlayer.equals(randomKi)) {
@@ -93,6 +99,7 @@ public class Controller {
             System.out.println("Game Over!\nPlayer1 = " + player1.getScore() + "\nPlayer2 = " + player2.getScore());
             player1.setIsActive(false);
             player2.setIsActive(false);
+            GameOver = true;
             PrintOut print = new PrintOut(gameField.getMatrixSize(), gameField.getBoxMatrix(), gameField.getLineMatrix());
             print.printMatrix();
             System.out.println(this.toString());
@@ -102,13 +109,8 @@ public class Controller {
     /**
      * Wird vom Gui aufgerufen. Initialisiert alle benötigten Klassen für ein
      * neues Spiel. Noch nicht implementiert: (Je nach gameModi werden die
-     * Spieler unterschiedlich initialisiert): 
-     *                  player 1    player2 
-     * 0(default)   ->  KI          KI 
-     * 1            ->  KI          GUI 
-     * 2            ->  GUI         KI 
-     * 3            ->  GUI         GUI
-     * etc.
+     * Spieler unterschiedlich initialisiert): player 1 player2 0(default) -> KI
+     * KI 1 -> KI GUI 2 -> GUI KI 3 -> GUI GUI etc.
      *
      * @param gameModi
      * @param size
@@ -122,6 +124,7 @@ public class Controller {
         gameField = new GameField(size);
         gameVariant = new GameVariant(gameModi);
         randomKi = new RandomKI(gameField);
+        GameOver = false;
 
         switch (gameModi) {
             case 1:
@@ -138,7 +141,7 @@ public class Controller {
                 this.player1 = new Player(player1, color1, gui, true);
                 this.player2 = new Player(player2, color2, gui, false);
                 break;
-                
+
             default:
                 this.player1 = new Player(player1, color1, randomKi, true);
                 this.player2 = new Player(player2, color2, randomKi, false);
@@ -149,19 +152,33 @@ public class Controller {
         nextMove();
 
     }
-    
+
     @Override
-    public String toString(){
+    public String toString() {
         String string;
-        string = gameVariant.getModi() + " " + (gameField.getMatrixSize() - 1)/2 + " " + player1.getName() + " " + player2.getName();
+        string = gameVariant.getModi() + " " + (gameField.getMatrixSize() - 1) / 2 + " " + player1.getName() + " " + player2.getName();
         return string;
     }
-    
-    public Player getPayer1(){
-    
+
+    /**
+     * Zeit dem Gui an wer Spieler eins ist ( wird benötigt um die Boxen richtig
+     * zuzuweisen)
+     *
+     * @return den player1
+     */
+    public Player getPayer1() {
+
         return player1;
     }
-    
+
+    /**
+     * Zeigt dem GUI an dass das Spiel beendet ist
+     *
+     * @return false wenn Spiel noch läuft, true wenn vorbei.
+     */
+    public boolean isGameOver() {
+        return GameOver;
+    }
 
     public static void main(String[] args) {
         Controller controller = new Controller();
