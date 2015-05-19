a71fa756dedc3144a12e09114b70b7056ca7c9bb
Adrian <Adrian@10.3.112.179>
2015-05-19 14:28:38 +0200

    network

--- DotsandBoxes/src/ch/hslu/prg2/network/NetworkOpponent.java
+++ DotsandBoxes/src/ch/hslu/prg2/network/NetworkOpponent.java
@@ -5,6 +5,11 @@
  */
 package ch.hslu.prg2.network;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
@@ -16,14 +21,18 @@ import java.net.InetAddress;
 public class NetworkOpponent {
 
     private int port = 0;
+    
+    public NetworkOpponent(){
+    }
 
     public void server(int port) {
+        boolean connection = false;
         try (DatagramSocket socket = new DatagramSocket(port)) {
-            while (true) {
+            while (!connection) {
                 DatagramPacket packet = new DatagramPacket(new byte[1024], 1024);
                 socket.receive(packet);
                 InetAddress address = packet.getAddress();
-                int port = packet.getPort();
+                //int port = packet.getPort();
                 int len = packet.getLength();
                 byte[] data = packet.getData();
                 //data = new Date().toString().getBytes();
@@ -37,11 +46,9 @@ public class NetworkOpponent {
 
     public void client() {
         try (DatagramSocket socket = new DatagramSocket()) {
-            InetAddress address
-                    = InetAddress.getByName("localhost");
+            InetAddress address = InetAddress.getByName("localhost");
             byte[] raw = new byte[1024];
-            DatagramPacket packet = new DatagramPacket(
-                    raw, raw.length, address, 13);
+            DatagramPacket packet = new DatagramPacket(raw, raw.length, address, 13);
             socket.send(packet);
             socket.receive(packet);
             int len = packet.getLength();
@@ -51,5 +58,33 @@ public class NetworkOpponent {
             System.err.println("Error: " + e.getMessage());
         }
     }
+    
+    public byte[] read(File file) throws IOException {
 
+    ByteArrayOutputStream ous = null;
+    InputStream ios = null;
+    try {
+        byte[] buffer = new byte[1024];
+        ous = new ByteArrayOutputStream();
+        ios = new FileInputStream(file);
+        int read = 0;
+        while ( (read = ios.read(buffer)) != -1 ) {
+            ous.write(buffer, 0, read);
+        }
+    } finally { 
+        try {
+             if ( ous != null ) 
+                 ous.close();
+        } catch ( IOException e) {
+        }
+
+        try {
+             if ( ios != null ) 
+                  ios.close();
+        } catch ( IOException e) {
+        }
+    }
+    return ous.toByteArray();
+    }
 }
+
