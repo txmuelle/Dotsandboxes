4a41afa54faef0fac859dad5b2e21925bdd4d752
Adrian <Adrian@10.3.112.179>
2015-05-19 13:22:49 +0200

    GameVariant erstellt

--- DotsandBoxes/src/ch/hslu/prg2/network/NetworkOpponent.java
+++ DotsandBoxes/src/ch/hslu/prg2/network/NetworkOpponent.java
@@ -5,10 +5,37 @@
  */
 package ch.hslu.prg2.network;
 
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+
 /**
  *
- * @author Müller Urs
+ * @author Adrian Ruckli, Patrick Rossacher
  */
+
 public class NetworkOpponent {
     
+    private int port = 0;
+    
+    public 
+
+    public void server(int port){
+        try (DatagramSocket socket = new DatagramSocket(port)) {
+            while (true) {
+                DatagramPacket packet = new DatagramPacket(new byte[1024], 1024);
+                socket.receive(packet);
+                InetAddress address = packet.getAddress();
+                int port = packet.getPort();
+                int len = packet.getLength();
+                byte[] data = packet.getData();
+                //data = new Date().toString().getBytes();
+                packet = new DatagramPacket(data, data.length, address, port);
+                socket.send(packet);
+            }
+        } catch (Exception e) {
+            System.err.println("Error: " + e.getMessage());
+        }
+    }
 }
